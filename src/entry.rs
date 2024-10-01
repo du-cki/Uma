9e2e176583682583a7927cf1b350fbd0dadb973d
du-cki <qwuakers@gmail.com>
2024-10-02 00:12:06 +0400

    consistent line spacing

--- src/entry.rs
+++ src/entry.rs
@@ -7,45 +7,63 @@ use crate::{
 
 use super::colors::*;
 
-fn println(num: usize, s: &str) {
-    println!("  {} {}", format!("{} |", num).blue(), s);
+macro_rules! print_line {
+    ($line_num:expr, $msg:expr, $width:expr) => {{
+        let line_num = $line_num;
+
+        println!(
+            "  {} {}",
+            format!("{:width$} |", line_num, width = $width).blue(),
+            $msg
+        );
+    }};
+
+    ($msg:expr, $width:expr) => {{
+        println!("  {} {}", " ".repeat($width), $msg);
+    }};
+
+    ($msg:expr) => {{
+        println!("  {}", $msg);
+    }};
 }
 
 fn error(err: &ParserError, source: &str, file_name: &str) {
     let lines: Vec<&str> = source.lines().collect();
     let error_line = err.token.line;
 
-    println!(
-        " {} {}:{}:{}:",
+    let max_line_num = (error_line + 2).min(lines.len());
+    let line_num_width = max_line_num.to_string().len();
+
+    print_line!(format!(
+        " {} {}:{}:{}",
         "-->".blue(),
         file_name,
         error_line,
         err.token.column
-    );
+    ));
 
     if error_line > 2 {
-        println(error_line - 2, lines[error_line - 3]);
+        print_line!(error_line - 2, lines[error_line - 3], line_num_width)
     }
 
     if error_line > 1 {
-        println(error_line - 1, lines[error_line - 2]);
+        print_line!(error_line - 1, lines[error_line - 2], line_num_width);
     }
 
-    println(error_line, lines[error_line - 1]);
+    print_line!(error_line, lines[error_line - 1], line_num_width);
 
-    println!(
-        "{} {} {}",
-        "    |".blue(),
-        " ".repeat(err.token.column),
+    print_line!(format!(
+        " {} {}",
+        " ".repeat(err.token.column + line_num_width),
         format!("^ {}", err.message).red()
-    );
+    ));
 
     if error_line < lines.len() {
-        println(error_line + 1, lines[error_line]);
+        print_line!(error_line + 1, lines[error_line], line_num_width);
     }
 
     if error_line + 1 < lines.len() {
-        println(error_line + 2, lines[error_line + 1]);
+        print_line!(error_line + 2, lines[error_line + 1], line_num_width);
     }
 
     std::process::exit(1);
